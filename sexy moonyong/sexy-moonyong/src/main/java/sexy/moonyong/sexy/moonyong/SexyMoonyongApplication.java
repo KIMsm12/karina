fe0d3000e42083a6697ea37ddc7f01575aee25d4
KimuSuMin <102895295+KIMsm12@users.noreply.github.com>
2025-01-16 22:03:34 +0900

    up

---
+++ sexy moonyong/sexy-moonyong/src/main/java/sexy/moonyong/sexy/moonyong/SexyMoonyongApplication.java
@@ -0,0 +1,13 @@
+package sexy.moonyong.sexy.moonyong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SexyMoonyongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SexyMoonyongApplication.class, args);
+	}
+
+}
---
+++ sexy moonyong/sexy-moonyong/src/test/java/sexy/moonyong/sexy/moonyong/SexyMoonyongApplicationTests.java
@@ -0,0 +1,13 @@
+package sexy.moonyong.sexy.moonyong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SexyMoonyongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
